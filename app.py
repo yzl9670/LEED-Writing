b4e41cfff061139fdb0acbd26b67254f020a087f
YZL <zhlyang9670@gmail.com>
2025-01-29 18:26:29 -0500

    Removed OpenAI API key from Git history

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from feedback import get_feedback, process_leed_items, collection
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
-app.config['SQLALCHEMY_DATABASE_URI'] = 'ssqlite:///instance/users.db'  # Adjust as needed
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/users.db'  # Adjust as needed
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Configure file uploads
